f3e1576810d43ead52818829caf10ed84c8aa191
lambiengcode <lambiengcode@gmail.com>
2023-07-26 14:57:17 +0700

    update .

--- modules/audio_processing/ns/rnnoise/src/rnn.h
+++ modules/audio_processing/ns/rnnoise/src/rnn.h
@@ -28,31 +28,30 @@
 #define RNN_H_
 
 #include "../include/rnnoise.h"
-
 #include "opus_types.h"
 
-#define WEIGHTS_SCALE (1.f/256)
+#define WEIGHTS_SCALE (1.f / 256)
 
 #define MAX_NEURONS 128
 
-#define ACTIVATION_TANH    0
+#define ACTIVATION_TANH 0
 #define ACTIVATION_SIGMOID 1
-#define ACTIVATION_RELU    2
+#define ACTIVATION_RELU 2
 
 typedef signed char rnn_weight;
 
 typedef struct {
-  const rnn_weight *bias;
-  const rnn_weight *input_weights;
+  const rnn_weight* bias;
+  const rnn_weight* input_weights;
   int nb_inputs;
   int nb_neurons;
   int activation;
 } DenseLayer;
 
 typedef struct {
-  const rnn_weight *bias;
-  const rnn_weight *input_weights;
-  const rnn_weight *recurrent_weights;
+  const rnn_weight* bias;
+  const rnn_weight* input_weights;
+  const rnn_weight* recurrent_weights;
   int nb_inputs;
   int nb_neurons;
   int activation;
@@ -60,10 +59,12 @@ typedef struct {
 
 typedef struct RNNState RNNState;
 
-void compute_dense(const DenseLayer *layer, float *output, const float *input);
+void rnnoise_compute_dense(const DenseLayer* layer,
+                           float* output,
+                           const float* input);
 
-void compute_gru(const GRULayer *gru, float *state, const float *input);
+void rnnoise_compute_gru(const GRULayer* gru, float* state, const float* input);
 
-void compute_rnn(RNNState *rnn, float *gains, float *vad, const float *input);
+void compute_rnn(RNNState* rnn, float* gains, float* vad, const float* input);
 
 #endif /* RNN_H_ */
