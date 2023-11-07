60a68094dcce96abfcddafd221c79c6134a35e85
lambiengcode <lambiengcode@gmail.com>
2023-11-07 14:25:04 +0700

    fix: error: no member named 'kPictureIdUri' in 'webrtc::RtpExtension'

--- api/rtp_parameters.cc
+++ api/rtp_parameters.cc
@@ -136,7 +136,6 @@ constexpr char RtpExtension::kRidUri[];
 constexpr char RtpExtension::kRepairedRidUri[];
 constexpr char RtpExtension::kVideoFrameTrackingIdUri[];
 constexpr char RtpExtension::kCsrcAudioLevelsUri[];
-constexpr char RtpExtension::kPictureIdUri[];
 
 constexpr int RtpExtension::kMinId;
 constexpr int RtpExtension::kMaxId;
@@ -172,8 +171,7 @@ bool RtpExtension::IsSupportedForVideo(absl::string_view uri) {
          uri == webrtc::RtpExtension::kRidUri ||
          uri == webrtc::RtpExtension::kRepairedRidUri ||
          uri == webrtc::RtpExtension::kVideoLayersAllocationUri ||
-         uri == webrtc::RtpExtension::kVideoFrameTrackingIdUri ||
-		 uri == webrtc::RtpExtension::kPictureIdUri;
+         uri == webrtc::RtpExtension::kVideoFrameTrackingIdUri;
 }
 
 bool RtpExtension::IsEncryptionSupported(absl::string_view uri) {
