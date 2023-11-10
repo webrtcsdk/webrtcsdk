2ec6106174b4ebcac1844b1edcaec740cfd54dd2
lambiengcode <lambiengcode@gmail.com>
2023-11-11 00:37:51 +0700

    fix: common_video/h265/h265_common.cc:30 crash app

--- common_video/h265/h265_common.cc
+++ common_video/h265/h265_common.cc
@@ -16,8 +16,7 @@ namespace H265 {
 
 const uint8_t kNaluTypeMask = 0x7E;
 
-std::vector<NaluIndex> FindNaluIndices(const uint8_t* buffer,
-                                           size_t buffer_size) {
+std::vector<NaluIndex> FindNaluIndices(const uint8_t* buffer, size_t buffer_size) {
   std::vector<NaluIndex> sequences;
   if (buffer_size < kNaluShortStartSequenceSize)
     return sequences;
@@ -27,11 +26,11 @@ std::vector<NaluIndex> FindNaluIndices(const uint8_t* buffer,
 
   const size_t end = buffer_size - kNaluShortStartSequenceSize;
   for (size_t i = 0; i < end;) {
-    if (buffer[i + 2] == 0) {
-      if (buffer[i + 1] == 0 && buffer[i] == 1) {
+    if (i + 2 < buffer_size && buffer[i + 2] == 0) {
+      if (i + 1 < buffer_size && i < buffer_size && buffer[i + 1] == 0 && buffer[i] == 1) {
         // We found a start sequence, now check if it was a 3 or 4-byte one.
         NaluIndex index = {i, i + 3, 0};
-        if (index.start_offset > 0 && buffer[index.start_offset - 1] == 0)
+        if (index.start_offset > 0 && index.start_offset < buffer_size && buffer[index.start_offset - 1] == 0)
           --index.start_offset;
 
         // Update length of previous entry.
@@ -41,7 +40,7 @@ std::vector<NaluIndex> FindNaluIndices(const uint8_t* buffer,
 
         sequences.push_back(index);
         i += 3;
-      } else if (buffer[i + 1] == 0 && buffer[i] == 0 && buffer[i + 3] == 1) {
+      } else if (i + 1 < buffer_size && i < buffer_size && i + 3 < buffer_size && buffer[i + 1] == 0 && buffer[i] == 0 && buffer[i + 3] == 1) {
         // 4-byte start sequence
         NaluIndex index = {i, i + 4, 0};
 
