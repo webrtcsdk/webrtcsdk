bd18131db1da0ee84ab299e7c5dc7e3a3f5cfc91
lambiengcode <lambiengcode@gmail.com>
2023-11-15 10:11:34 +0700

    fix: try/catch

--- common_video/h264/h264_common.cc
+++ common_video/h264/h264_common.cc
@@ -18,52 +18,46 @@ namespace H264 {
 const uint8_t kNaluTypeMask = 0x1F;
 
 std::vector<NaluIndex> FindNaluIndices(const uint8_t* buffer,
-                                       size_t buffer_size) {
+                                       size_t buffer_size) noexcept {
   // This is sorta like Boyer-Moore, but with only the first optimization step:
   // given a 3-byte sequence we're looking at, if the 3rd byte isn't 1 or 0,
   // skip ahead to the next 3-byte sequence. 0s and 1s are relatively rare, so
   // this will skip the majority of reads/checks.
   std::vector<NaluIndex> sequences;
-  
-  try {
-    if (buffer_size < kNaluShortStartSequenceSize)
-      return sequences;
+  if (buffer_size < kNaluShortStartSequenceSize)
+    return sequences;
 
-    static_assert(kNaluShortStartSequenceSize >= 2,
-                  "kNaluShortStartSequenceSize must be larger or equals to 2");
-    const size_t end = buffer_size - kNaluShortStartSequenceSize;
-    for (size_t i = 0; i < end;) {
-      if (buffer[i + 2] > 1) {
-        i += 3;
-      } else if (buffer[i + 2] == 1) {
-        if (buffer[i + 1] == 0 && buffer[i] == 0) {
-          // We found a start sequence, now check if it was a 3 of 4 byte one.
-          NaluIndex index = {i, i + 3, 0};
-          if (index.start_offset > 0 && buffer[index.start_offset - 1] == 0)
-            --index.start_offset;
+  static_assert(kNaluShortStartSequenceSize >= 2,
+                "kNaluShortStartSequenceSize must be larger or equals to 2");
+  const size_t end = buffer_size - kNaluShortStartSequenceSize;
+  for (size_t i = 0; i < end;) {
+    if (buffer[i + 2] > 1) {
+      i += 3;
+    } else if (buffer[i + 2] == 1) {
+      if (buffer[i + 1] == 0 && buffer[i] == 0) {
+        // We found a start sequence, now check if it was a 3 of 4 byte one.
+        NaluIndex index = {i, i + 3, 0};
+        if (index.start_offset > 0 && buffer[index.start_offset - 1] == 0)
+          --index.start_offset;
 
-          // Update length of previous entry.
-          auto it = sequences.rbegin();
-          if (it != sequences.rend())
-            it->payload_size = index.start_offset - it->payload_start_offset;
+        // Update length of previous entry.
+        auto it = sequences.rbegin();
+        if (it != sequences.rend())
+          it->payload_size = index.start_offset - it->payload_start_offset;
 
-          sequences.push_back(index);
-        }
-
-        i += 3;
-      } else {
-        ++i;
+        sequences.push_back(index);
       }
-    }
 
-    // Update length of last entry, if any.
-    auto it = sequences.rbegin();
-    if (it != sequences.rend())
-      it->payload_size = buffer_size - it->payload_start_offset;
-  } catch (const std::exception& e) {
-     // Handle the exception (log, rethrow, etc.)
-     std::cerr << "Exception caught: " << e.what() << std::endl;
-   }
+      i += 3;
+    } else {
+      ++i;
+    }
+  }
+
+  // Update length of last entry, if any.
+  auto it = sequences.rbegin();
+  if (it != sequences.rend())
+    it->payload_size = buffer_size - it->payload_start_offset;
 
   return sequences;
 }
